d63f61a0c2c6b3ebaafb452b631d96c52b4ea44b
Soyann <chensyabc@163.com>
2019-10-17 20:57:36 +0800

    #Commit UrlUtil and Tables definition

---
+++ GPai_Tables.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE IF NOT EXISTS Auction_Spider_GPai;
+USE Auction_Spider_GPai;
+
+DROP TABLE IF EXISTS Auctions;
+CREATE TABLE IF NOT EXISTS Auctions
+(
+  `Id` INT AUTO_INCREMENT,
+  `AuctionId` INT NOT NULL,
+  `CourtId` INT NOT NULL,
+  `Title` VARCHAR(100) NOT NULL,
+  `CategoryId` INT NOT NULL,
+  `Url` VARCHAR(80) NOT NULL,
+  `StartPrice` VARCHAR(80) NOT NULL,
+  `CurrentPrice` VARCHAR(80) DEFAULT NULL,
+  `CashDeposit` VARCHAR(80) DEFAULT NULL,
+  `PaymentAdvance` VARCHAR(80) DEFAULT NULL,
+  `AccessPrice` VARCHAR(80) DEFAULT NULL,
+  `FareIncrease` VARCHAR(80) DEFAULT NULL,
+  `AuctionTimes` VARCHAR(10) DEFAULT NULL,
+  `AuctionType` VARCHAR(20) DEFAULT NULL,
+  `DelayCycle` VARCHAR(80) DEFAULT NULL,
+  `CorporateAgent` VARCHAR(30) DEFAULT NULL,
+  `Phone` VARCHAR(50) DEFAULT NULL,
+  `SellingPeriod` VARCHAR(30) DEFAULT NULL,
+  `OnlineCycle` VARCHAR(10) DEFAULT NULL,
+  `BiddingRecord` VARCHAR(10) DEFAULT NULL,
+  `AuctionModel` VARCHAR(10) DEFAULT NULL,
+  `Enrollment` VARCHAR(10) DEFAULT NULL,
+  `SetReminders` VARCHAR(10) DEFAULT NULL,
+  `Onlookers` VARCHAR(10) DEFAULT NULL,
+  `CreatedOn` DATE DEFAULT NULL,
+  `UpdatedOn` DATE DEFAULT NULL,
+  `Status` INT NOT NULL,
+  `SpiderStatus` INT NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS Auction_Categories;
+CREATE TABLE IF NOT EXISTS Auction_Categories
+(
+  `Id` INT AUTO_INCREMENT,
+  `CategoryId` VARCHAR(10) NOT NULL,
+  `CategoryName` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS Auction_Statuses;
+CREATE TABLE IF NOT EXISTS Auction_Statuses
+(
+  `Id` INT AUTO_INCREMENT,
+  `StatusId` INT NOT NULL,
+  `StatusName` VARCHAR(15) NOT NULL,
+  `IsSpiderThisStatus` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS Courts;
+CREATE TABLE IF NOT EXISTS Courts
+(
+  `Id` INT AUTO_INCREMENT,
+  `CourtId` INT NOT NULL,
+  `CourtName` VARCHAR(60) NOT NULL,
+  `City` VARCHAR(60) DEFAULT NULL,
+  `Province` VARCHAR(60) DEFAULT NULL,
+  `AuctionCount` INT NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO Auction_Statuses(StatusId, StatusName, IsSpiderThisStatus)
+    SELECT 1, N'即将拍卖', 1
+    UNION SELECT 2, N'正在拍卖', 1
+    UNION SELECT 3, N'已成交', 1
+    UNION SELECT 4, N'其他', 1
+
+INSERT INTO auction_categories(CategoryId, CategoryName)
+    SELECT 372, N'股权'
+    UNION SELECT 376, N'房产'
+    UNION SELECT 377, N'物资'
+    UNION SELECT 378, N'机动车'
+    UNION SELECT 379, N'其他'
+    UNION SELECT 380, N'工艺品'
+    UNION SELECT 381, N'土地'
+    UNION SELECT 383, N'船舶'
+    UNION SELECT 385, N'无形资产'
+    UNION SELECT 386, N'财产性权益'
+
